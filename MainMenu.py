4738e72bea995fdbbf9ec89aa0d5bae04cbb20ab
Seamus Walden <37228858+8BitToaster@users.noreply.github.com>
2019-03-17 14:13:08 -0400

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -28,12 +28,27 @@ DisplayWidth,DisplayHeight = 1000, 800
 clock = pygame.time.Clock()
 
 gameDisplay = pygame.display.set_mode((DisplayWidth,DisplayHeight))
-pygame.display.set_caption("Name")
+pygame.display.set_caption("Fruit Slasher")
 font_100 = pygame.freetype.Font("Font.ttf", 100)
 font_50 = pygame.freetype.Font("Font.ttf", 50)
 font_75 = pygame.freetype.Font("Font.ttf", 75) 
 SizeCheck = pygame.font.Font("Font.ttf", 50)
 SizeCheck_75 = pygame.font.Font("Font.ttf", 75)
+font_35 = pygame.freetype.Font("Font.ttf", 35)
+
+#Loading the images
+def load_images(path_to_directory):
+    images = {}
+    for dirpath, dirnames, filenames in os.walk(path_to_directory):
+        for name in filenames:
+            if name.endswith('.png'):
+                key = name[:-4]
+                if key != "Bg":
+                    img = pygame.image.load(os.path.join(dirpath, name)).convert_alpha()
+                else:
+                    img = pygame.image.load(os.path.join(dirpath, name)).convert()
+                images[key] = img
+    return images
 
 #The Buttons
 class Button():
@@ -52,11 +67,14 @@ class Button():
             pygame.draw.rect(gameDisplay,(250,0,0),(self.x,self.y,self.width,self.height),0)
         pygame.draw.rect(gameDisplay,(100,100,100),(self.x,self.y,self.width,self.height),5)
 
-        text_surface, rect = font_50.render(str(self.text), (0, 0, 0))
-        if self.text != "Options":
+        if self.text != "Change Color":
+            text_surface, rect = font_50.render(str(self.text), (0, 0, 0))
             gameDisplay.blit(text_surface, (self.x + int(self.width/2)+30 - int(SizeCheck.size(str(self.text))[0]), self.y + int(self.height/2) - 20))
         else:
-            gameDisplay.blit(text_surface, (self.x + int(self.width/2)+43 - int(SizeCheck.size(str(self.text))[0]), self.y + int(self.height/2) - 20))
+            text_surface, rect = font_35.render(str("Change"), (0, 0, 0))
+            gameDisplay.blit(text_surface,(565,615))
+            text_surface, rect = font_35.render(str("Color"), (0, 0, 0))
+            gameDisplay.blit(text_surface,(585,650))
 
 def shorten(Num):
     count = 0
@@ -90,33 +108,67 @@ def shorten(Num):
 
 def HomeScreen(score=0):
     game_run = True
-    Buttons = [Button(400,600,200,100,"Play")]
+    Buttons = [Button(275,600,200,100,"Play"),Button(525,600,200,100,"Change Color")]
+    CcButtons = [Button(750,650,200,100,"Exit"),Button(150,250,50,50,""),Button(800,250,50,50,"")]
+    screen = "Main"
+    Colors = [(0,250,0),(250,0,0),(0,0,250),(255,255,0),(0,255,255)]
+    SubColors = [(0,150,0),(150,0,0),(0,0,150),(150,150,0),(0,150,150)]
+    ColorSelection = 0
+    Images = load_images("Images")
     
     while game_run == True:
 
         gameDisplay.fill((210,140,42))
+        #gameDisplay.blit(pygame.transform.scale(Images["Bg"],(DisplayWidth,DisplayHeight)),(0,0))
         pos = pygame.mouse.get_pos()
-        text_surface, rect = font_100.render(("Fruit Slasher"), (0, 0, 0))
-        gameDisplay.blit(text_surface, (220, 100))
+        if screen == "Main":
+            text_surface, rect = font_100.render(("Fruit Slasher"), (0, 0, 0))
+            gameDisplay.blit(text_surface, (220, 100))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                for i, button in enumerate(Buttons):
-                    if button.x <= pos[0] <= button.x + button.width and button.y <= pos[1] <= button.y + button.height:
-                        if i == 0:
-                            main.game_loop()
+                if screen == "Main":
+                    for i, button in enumerate(Buttons):
+                        if button.x <= pos[0] <= button.x + button.width and button.y <= pos[1] <= button.y + button.height:
+                            if i == 0:
+                                main.game_loop([Colors[ColorSelection],SubColors[ColorSelection]])
+                            if i == 1:
+                                screen = "Change Color"
+                if screen == "Change Color":
+                    for i, button in enumerate(CcButtons):
+                        if button.x <= pos[0] <= button.x + button.width and button.y <= pos[1] <= button.y + button.height:
+                            if i == 0:
+                                screen = "Main"
+                            if i == 1:
+                                ColorSelection += 1
+                                ColorSelection %= (len(Colors))
+                            if i == 2:
+                                ColorSelection -= 1
+                                if ColorSelection < 0:
+                                    ColorSelection = len(Colors)-1
+                                
 
-        if score != 0:
-            text_surface, rect = font_75.render(("Score: " + shorten(score)), (0, 0, 0))
-            gameDisplay.blit(text_surface, (480 - SizeCheck_75.size(shorten(score))[0], 300))
+        if screen == "Main":
+            if score != 0:
+                text_surface, rect = font_75.render(("Score: " + shorten(score)), (0, 0, 0))
+                gameDisplay.blit(text_surface, (440 - SizeCheck_75.size(shorten(score))[0], 300))
 
 
-        #Updating the buttons
-        for button in Buttons:
-            button.draw()
+            #Updating the buttons
+            for button in Buttons:
+                button.draw()
+
+        if screen == "Change Color":
+            for button in CcButtons:
+                button.draw()
+            pygame.draw.rect(gameDisplay,Colors[ColorSelection],(350,200,300,150),0)
+            pygame.draw.rect(gameDisplay,SubColors[ColorSelection],(350,200,300,150),5)
+            pygame.draw.polygon(gameDisplay,(150,150,150),[(808,259),(808,289),(838,274)],0)
+            pygame.draw.polygon(gameDisplay,(150,150,150),[(192,259),(192,289),(162,274)],0)
+        
 
         pygame.display.flip()
         clock.tick(60)
